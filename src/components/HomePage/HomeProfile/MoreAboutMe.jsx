b12fef0c6351f24da9f3e038d10d3e3f9ff8dbba
Jamel Ariza <jamelqui@hotmail.com>
2023-10-09 09:30:26 -0500

    Actualizamos acceso a carpeta images

--- src/components/HomePage/HomeProfile/MoreAboutMe.jsx
+++ src/components/HomePage/HomeProfile/MoreAboutMe.jsx
@@ -1,7 +1,7 @@
 const MoreAboutMe = () => {
   return (
     <div className="moreaboutme row">
-      <h2 className="text-center pt-3 pb-4">Más Sobre Mi</h2>
+      <h2 className="text-center pb-4">Más Sobre Mi</h2>
       <div className="images row justify-content-center gap-3">
         <div className="image col-sm-5">
           <img src="./images/header-jm.png" alt="" />
