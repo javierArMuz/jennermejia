90fc5b8d4c7bb7bee226a57aa42136ee8ae21908
Jamel Ariza <jamelqui@hotmail.com>
2023-10-11 08:44:28 -0500

    actualizamos instagram

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const MyFooter = () => {
       <Link to='https://www.facebook.com/jennermejiaperez' target='_blank' className='icon'>
         <i className="bi bi-facebook"></i>
       </Link>
-      <Link to='https://www.instagram.com/jenner_mejia' target='_blank' className='icon'>
+      <Link to='https://www.instagram.com/jenner_mejiap' target='_blank' className='icon'>
         <i className="bi bi-instagram"></i>
       </Link>
       <Link to='https://wa.me/573006058289' target='_blank' className='icon'>
